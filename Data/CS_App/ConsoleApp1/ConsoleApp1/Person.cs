611ce9675161df080d8b65433bd8ea3fbee1455b
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-05-12 11:52:37 +0800

    Day1

---
+++ Data/CS_App/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        private string id, name;
+        public Person(string id, string name)
+        { this.id = id; this.name = name;}
+
+        public void Display()
+        {
+            Console.WriteLine($"id={id}, name={name}");
+        }
+    }
+}
---
+++ Data/CS_App/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+/*
+string id, name;
+id = "C001";
+name = "John";
+Console.WriteLine($"id={id}, name={name}");
+*/
+
+using ConsoleApp1;
+
+Person p1 = new Person("C001", "John");
+p1.Display();
